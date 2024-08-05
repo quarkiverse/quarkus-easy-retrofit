4b4e9999050b47e3a2a779383d3d2f0321af72d4
liuziyuan <liuziyuan.room@gmail.com>
2024-08-05 13:06:47 +0800

    try to fix proxy issue

--- integration-tests/src/main/java/io/quarkiverse/quarkus/easy/retrofit/it/api/BaseApi.java
+++ integration-tests/src/main/java/io/quarkiverse/quarkus/easy/retrofit/it/api/BaseApi.java
@@ -4,10 +4,12 @@ import io.github.liuziyuan.retrofit.core.annotation.RetrofitBuilder;
 import io.quarkiverse.quarkus.easy.retrofit.it.api.retrofit.BodyCallAdapterFactoryBuilder;
 import io.quarkiverse.quarkus.easy.retrofit.it.api.retrofit.GuavaCallAdapterFactoryBuilder;
 import io.quarkiverse.quarkus.easy.retrofit.it.api.retrofit.JacksonConvertFactoryBuilder;
+import io.quarkus.runtime.annotations.RegisterForProxy;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
+@RegisterForProxy
 @RetrofitBuilder(baseUrl = "${backend.base.url}", addConverterFactory = {
         JacksonConvertFactoryBuilder.class }, addCallAdapterFactory = { BodyCallAdapterFactoryBuilder.class,
                 GuavaCallAdapterFactoryBuilder.class })
