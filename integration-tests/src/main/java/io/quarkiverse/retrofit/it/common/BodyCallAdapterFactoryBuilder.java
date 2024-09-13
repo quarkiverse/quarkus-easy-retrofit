58b53530687aaa002b0ff4d2e29f078645f36c90
liuziyuan <liuziyuan.room@gmail.com>
2024-09-13 10:03:32 +0800

    fix upgrade issue

--- integration-tests/src/main/java/io/quarkiverse/retrofit/it/common/BodyCallAdapterFactoryBuilder.java
+++ integration-tests/src/main/java/io/quarkiverse/retrofit/it/common/BodyCallAdapterFactoryBuilder.java
@@ -2,14 +2,14 @@ package io.quarkiverse.retrofit.it.common;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import io.github.easyretrofit.adapter.simplebody.SimpleBodyCallAdapterFactory;
 import io.github.easyretrofit.core.builder.BaseCallAdapterFactoryBuilder;
-import io.github.liuziyuan.retrofit.adapter.simple.body.BodyCallAdapterFactory;
 import retrofit2.CallAdapter;
 
 @ApplicationScoped
 public class BodyCallAdapterFactoryBuilder extends BaseCallAdapterFactoryBuilder {
     @Override
     public CallAdapter.Factory buildCallAdapterFactory() {
-        return BodyCallAdapterFactory.create();
+        return SimpleBodyCallAdapterFactory.create();
     }
 }
